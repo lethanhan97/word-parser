f8c99d10c8709e49744c09b11b583fdefb5389f3
An Le Thanh <anlethanh@Ans-MacBook-Pro.local>
2021-02-25 23:01:35 +0800

    added file reader

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import { createRef, FormEvent } from 'react';
 import './App.css';
 
 function App() {
+  const fileRef = createRef<HTMLInputElement>();
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const currentFile = fileRef.current?.files?.item(0);
+
+    if (!currentFile) {
+      alert("You didn't put in a file...");
+      return;
+    }
+    const fileIsValid = checkType(currentFile as File);
+    if (!fileIsValid) {
+      alert('You put in the wrong type of file');
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.readAsText(currentFile);
+    reader.onload = () => console.log(reader.result);
+  };
+
+  const checkType = (file: File) => {
+    const allowedTypes = ['text/plain'];
+    const currentType = file.type;
+
+    return allowedTypes.includes(currentType);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmit}>
+        <input type="file" id="myfile" name="myfile" ref={fileRef}></input>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
